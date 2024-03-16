0d9285485b22227096ebab9485c4e80763160e37
yoishi <75917699+yumadao@users.noreply.github.com>
2024-03-16 20:26:26 +0900

    turso導入

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,14 +3,23 @@ declare global {
 }
 
 import { PrismaClient } from "@prisma/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
+import { createClient } from "@libsql/client";
 
 let prisma: PrismaClient;
 
+const libsql = createClient({
+  url: `${process.env.TURSO_DATABASE_URL}`,
+  authToken: `${process.env.TURSO_AUTH_TOKEN}`,
+});
+
+const adapter = new PrismaLibSQL(libsql);
+
 if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
+  prisma = new PrismaClient({ adapter });
 } else {
   if (!global.prisma) {
-    global.prisma = new PrismaClient();
+    global.prisma = new PrismaClient({ adapter });
   }
   prisma = global.prisma;
 }
